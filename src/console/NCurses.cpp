22c74b7ac8c6813a3d296a019d695ca7e88bd243
Roudie <sebak19962@gmail.com>
2018-06-17 20:13:28 +0200

    ncurses

---
+++ src/console/NCurses.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 17.06.18.
+//
+
+#include "NCurses.h"
---
+++ src/console/include/NCurses.hpp
@@ -0,0 +1,14 @@
+//
+// Created by root on 17.06.18.
+//
+
+#ifndef LIFESIMULATION_NCURSES_H
+#define LIFESIMULATION_NCURSES_H
+
+
+class NCurses {
+
+};
+
+
+#endif //LIFESIMULATION_NCURSES_H
